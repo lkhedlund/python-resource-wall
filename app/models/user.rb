8a5438bdefe698c0605aa0ed07536379bdcfc5c8
livfwds <emiliehester@gmail.com>
2015-11-19 02:49:20 +0000

    Added associations and basic validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  has_many :articles
+  has_many :bookmarks
+  has_many :comments
+  has_many :likes
+
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i,
+    message: "Please enter a valid email address." } }, uniqueness: { case_sensitive: false }
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, length: {minimum: 6}
 end
