18c48579bd8592b41eeaeaf4db28af63228ebd1b
livfwds <emiliehester@gmail.com>
2015-11-19 03:19:55 +0000

    Added counter_cache to like and comment associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :article
+  belongs_to :user, counter_cache: true
+  belongs_to :article, counter_cache: true
 
   validates :comment, presence: true
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
 class Like < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :article
+  belongs_to :user, counter_cache: true
+  belongs_to :article, counter_cache: true
 end
