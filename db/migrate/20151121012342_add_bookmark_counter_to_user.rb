61036e74f55a70abccf582e15af1844a370b0bb2
JamesyK <jameskanegae@gmail.com>
2015-11-21 01:36:49 +0000

    another naming fix

--- db/migrate/20151121012342_add_bookmark_counter_to_user.rb
+++ db/migrate/20151121012342_add_bookmark_counter_to_user.rb
@@ -1,4 +1,4 @@
-class Add-bookmark-counter-to-user < ActiveRecord::Migration
+class AddBookmarkCounterToUser < ActiveRecord::Migration
   def change
     change_table :users do |t|
       t.integer :bookmarks_count
