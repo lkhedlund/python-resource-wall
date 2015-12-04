28b8a66aae7ed5513aaec719fba226c266d29ed6
lkhedlund <lkhedlund@gmail.com>
2015-12-03 21:19:28 -0800

    Model and migrations complete

--- posting_wall/models.py
+++ posting_wall/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    summary = models.TextField()
+    url = models.URLField(blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
