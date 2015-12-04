71752d3d77c5ad3e96950024f03b4442c5cf400b
lkhedlund <lkhedlund@gmail.com>
2015-12-04 13:41:37 -0800

    Form mostly finished. Need to add additional conditions

--- posting_wall/views.py
+++ posting_wall/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -7,5 +8,17 @@ def post_list(request):
     return render(request, 'posting_wall/post_list.html', { 'posts': posts })
 
 def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+    post = get_object_or_404( Post, pk=pk )
     return render(request, 'posting_wall/post_detail.html', { 'post': post })
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm( request.POST )
+        if form.is_valid():
+            post = form.save( commit=false )
+            post.author = request.user
+            post.save()
+            return redirect( 'post_detail', pk=post.pk )
+    else:
+        form = PostForm()
+    return render(request, 'posting_wall/post_edit.html', { 'form': form })
