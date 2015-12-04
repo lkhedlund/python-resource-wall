2085f2de6239c620e1859108bc2418c66284e939
lkhedlund <lkhedlund@gmail.com>
2015-12-03 21:10:57 -0800

    Django app initialized. Initial Upload

---
+++ posting_wall/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PostingWallConfig(AppConfig):
+    name = 'posting_wall'
---
+++ pywall/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\lars\dropbox\dev\code\python-resource-wall\pywall\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
