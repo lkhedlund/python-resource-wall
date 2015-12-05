5a3cf5c520c9c6cca2368e40b01ffe2dd4c12631
lkhedlund <lkhedlund@gmail.com>
2015-12-05 12:14:26 -0800

    User login added to most views

--- posting_wall/views.py
+++ posting_wall/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Post
 from .forms import PostForm
 
@@ -7,10 +8,12 @@ def post_list(request):
     posts = Post.objects.all()
     return render(request, 'posting_wall/post_list.html', { 'posts': posts })
 
+@login_required
 def post_detail(request, pk):
     post = get_object_or_404( Post, pk=pk )
     return render(request, 'posting_wall/post_detail.html', { 'post': post })
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm( request.POST )
@@ -23,6 +26,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'posting_wall/post_edit.html', { 'form': form })
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404( Post, pk=pk )
     if request.method == "POST":
