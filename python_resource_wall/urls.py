7e6e6f0730665e8e810ac982b97342f0e9f1c30f
lkhedlund <lkhedlund@gmail.com>
2015-12-05 12:38:23 -0800

    Commit before attempting to fix callables for login and out

--- python_resource_wall/urls.py
+++ python_resource_wall/urls.py
@@ -20,5 +20,6 @@ admin.autodiscover()
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'', include('posting_wall.urls')),
 ]
