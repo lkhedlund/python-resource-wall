06b48ee90866ccbc0a4bb4c2f10bf01bc8c90f2f
lkhedlund <lkhedlund@gmail.com>
2015-12-05 20:36:33 -0800

    Ready to deploy basic version

--- posting_wall/views.py
+++ posting_wall/views.py
@@ -8,7 +8,6 @@ def post_list(request):
     posts = Post.objects.all().order_by('-created_date')
     return render(request, 'posting_wall/post_list.html', { 'posts': posts })
 
-@login_required
 def post_detail(request, pk):
     post = get_object_or_404( Post, pk=pk )
     comments = post.comments.all().order_by('-created_date')
@@ -21,7 +20,7 @@ def post_detail(request, pk):
             comment.save()
     else:
         form = CommentForm()
-        return render(request, 'posting_wall/post_detail.html', { 'post': post, 'comments': comments, 'form': form })
+    return render(request, 'posting_wall/post_detail.html', { 'post': post, 'comments': comments, 'form': form })
 
 @login_required
 def post_new(request):
@@ -49,18 +48,3 @@ def post_edit(request, pk):
     else:
         form = PostForm( instance=post )
     return render(request, 'posting_wall/post_edit.html', { 'form': form })
-
-@login_required
-def post_comment_new(request, pk):
-    post = get_object_or_404( Post, pk=pk )
-    if request.method == "POST":
-        form = CommentForm( request.POST )
-        if form.is_valid():
-            comment = form.save( commit=False )
-            comment.author = request.user
-            comment.post = post
-            comment.save()
-            return redirect('posting_wall.views.post_detail', pk=post.pk )
-    else:
-        form = CommentForm()
-    return render(request, 'posting_wall/post_comment_new.html', { 'form':form })
