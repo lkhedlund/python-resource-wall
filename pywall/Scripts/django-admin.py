5645f5eed57ef8acaf464e9a5e5ab478c8d39d20
Lars Hedlund <lkhedlund@gmail.com>
2015-12-07 05:24:44 -0800

    Removed sinatra files and virtual env

--- pywall/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\lars\dropbox\dev\code\python-resource-wall\pywall\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
